70e1462db3e48ccaea3263b58193cd1d84ceda81
Msahk <90519489+Msahk@users.noreply.github.com>
2025-09-17 17:46:54 -0500

    login hecho
    marliton 17sep

--- 6to Trimestre/JSF/PAEV5/PAEV3/src/main/java/control/usuariosBean.java
+++ 6to Trimestre/JSF/PAEV5/PAEV3/src/main/java/control/usuariosBean.java
@@ -11,9 +11,11 @@ import javax.faces.context.FacesContext;
 
 import models.usuarios;
 import dao.usuariosDao;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @ManagedBean
@@ -23,32 +25,72 @@ public class usuariosBean {
     private final usuariosDao usuDAO = new usuariosDao();
     private usuarios usuario = new usuarios();
     private List<usuarios> lstUsu = new ArrayList<>();
+    
+    public usuarios getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(usuarios usuario) {
+        this.usuario = usuario;
+    }
 
     public void autenticar() {
-        try {
-            Connection con = ConDB.conectar();
-            
-            String sql = "SELECT * FROM usuarios WHERE correo = ? AND password = ?";
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, usuario.getCorreo());
-            String pw = Utilidades.encriptar(usuario.getPassword());
-            ps.setString(2, pw);
-            
-            ResultSet rs = ps.executeQuery();
-            
-            if(rs.next()) {
-                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("user", rs.getString("correo"));
-                switch (rs.getString("rol")) {
-                    case "A":
-                        FacesContext.getCurrentInstance().getExternalContext().redirect("dashboard.xhtml");
-                        break;
-                    default:
-                        throw new AssertionError();
-                }
-            } 
-        } catch (Exception e) {
+    
+    try {
+        Connection con = ConDB.conectar();
+
+        
+        String sql = "SELECT * FROM usuarios WHERE correo = ? OR documento = ? AND password = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, usuario.getCorreo());     
+        ps.setString(2, usuario.getCorreo());     
+        String pw = Utilidades.encriptar(usuario.getPassword());
+        ps.setString(3, pw);
+        
+        System.out.println(pw);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            System.out.println(">>> Login correcto: " + rs.getString("correo"));
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+                .put("user", rs.getString("correo"));
+
+            switch (rs.getString("rol")) {
+                case "A":
+                    FacesContext.getCurrentInstance().getExternalContext().redirect("dashboard.xhtml");
+                    break;
+                default:
+                    System.out.println(">>> Rol desconocido: " + rs.getString("rol"));
+                    break;
+            }
+        } else {
+            System.out.println(">>> Credenciales incorrectas");
+            FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_WARN, 
+                                 "Correo/Documento o contraseña no válidos", "Aviso"));
         }
+    } catch (Exception e) {
+        e.printStackTrace();
+        FacesContext.getCurrentInstance().addMessage(null, 
+            new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+                             "Error en la autenticación", "Error"));
     }
+}
+
+            public void logout() {
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+
+                FacesContext.getCurrentInstance().getExternalContext().redirect("../index.xhtml");
+
+                System.out.println("Sesión cerrada y redirigido a index");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     
     
     public void listar() {
@@ -71,13 +113,7 @@ public class usuariosBean {
         usuDAO.eliminar(usu);
     }
 
-    public usuarios getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(usuarios usuario) {
-        this.usuario = usuario;
-    }
+    
     
     public List<usuarios> getLstUsu() {
         return lstUsu;
